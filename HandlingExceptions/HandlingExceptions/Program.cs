a25c3a4ce0eb0057c2da4e779162a6d355dc94a7
Isidore <Isidore@AGOBIN>
2019-04-10 11:56:08 -0400

    Implemented Handling exceptions using the try catch block

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionTest();
+        }
+
+        private static void ExceptionTest()
+        {
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(@"C:\Users\Isidore\Desktop\data.txt");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch(FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ RecursiveFactorial/RecursiveFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result, n = 5;
+
+            result = Factorial(n);
+            Console.WriteLine("The factorial of {0} is {1}", n, result);
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
