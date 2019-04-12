f18353441bdab36ad274a70c28fcb70546f52c9e
Isidore <Isidore@AGOBIN>
2019-04-12 15:37:05 -0400

    Implemented bubblesort algorithm

---
+++ bubblesort/bubblesort/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers;
+            int n;
+            string s;
+
+            Console.Write("Enter number of numbers to sort: ");
+            
+            try
+            {
+                s = Console.ReadLine();
+                n = Convert.ToInt32(s);
+
+            }
+            catch (Exception ice)
+            {
+                Console.WriteLine("\nError message: " + ice.Message);
+                Console.WriteLine("End of program ...");
+                return; //Program terminates if user enters the wrong input, not a number
+            }
+
+            numbers = new int[n];
+
+            Console.Write("Enter " + n + " numbers to sort: ");
+
+            //Reads user input
+            for(int i = 0; i < n; i++)
+            {
+                int temp;
+
+                temp = Console.Read(); //Reads number from the input, assumes user enters the correct input
+
+                numbers[i] = temp; // int.Parse( //Convert.ToInt32(temp);
+            }
+
+            displayArray(numbers);
+            numbers = bubblesort(numbers);
+
+            
+        }
+
+        public static void displayArray(int[] numbers)
+        {
+            //Prints sorted numbers
+            Console.WriteLine();
+
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static int[] bubblesort(int[] numbers)
+        {
+            bool swapped = false;
+
+            do
+            {    
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    if(numbers[i] > numbers[i + 1])
+                    {
+                        int temp = numbers[i + 1];
+                        numbers[i + 1] = numbers[i];
+                        numbers[i] = temp;
+                        swapped = true;
+                    }
+
+                }
+            } while (swapped == false);
+
+            return numbers;
+        }
+    }
+}
